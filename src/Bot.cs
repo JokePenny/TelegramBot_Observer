734426ab746e48a35140782305b63cc29903327f
JokePenny <S-total-war@mail.ru>
2020-12-03 14:57:00 +0700

    fix path to secret key

--- src/Bot.cs
+++ src/Bot.cs
@@ -67,7 +67,7 @@ namespace TelegramBot_Observer.src
         {
             //Test on Windows 
             //FileStream fstream = File.OpenRead(Environment.CurrentDirectory + "\\password_bot.txt");
-            FileStream fstream = File.OpenRead("/home/root/secret_keys/password_bot.txt"); 
+            FileStream fstream = File.OpenRead("/root/secret_keys/password_bot.txt"); 
             byte[] array = new byte[fstream.Length];
             fstream.Read(array, 0, array.Length);
             fstream.Close();

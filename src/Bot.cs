eef5b5085fab9423ba46fe63fdb2034b6ec8f767
JokePenny <S-total-war@mail.ru>
2020-12-03 22:55:39 +0700

    fix

--- src/Bot.cs
+++ src/Bot.cs
@@ -62,9 +62,9 @@ namespace TelegramBot_Observer.src
         private string ReadDataPasswordsBot()
         {
             //Test on Windows 
-            FileStream fstream = File.OpenRead(Environment.CurrentDirectory + "\\password_bot.txt");
+            //FileStream fstream = File.OpenRead(Environment.CurrentDirectory + "\\password_bot.txt");
             //For server
-            //FileStream fstream = File.OpenRead("/home/lorne/secret_keys/password_bot.txt"); 
+            FileStream fstream = File.OpenRead("/root/secret_keys/password_bot.txt"); 
             byte[] array = new byte[fstream.Length];
             fstream.Read(array, 0, array.Length);
             fstream.Close();

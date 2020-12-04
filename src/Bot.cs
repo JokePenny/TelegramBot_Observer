c91be00c06e6caaff9d4d958034017f8ea151299
JokePenny <S-total-war@mail.ru>
2020-12-04 09:24:10 +0700

    change path to file

--- src/Bot.cs
+++ src/Bot.cs
@@ -67,7 +67,7 @@ namespace TelegramBot_Observer.src
             //Test on Windows 
             //FileStream fstream = File.OpenRead(Environment.CurrentDirectory + "\\password_bot.txt");
             //For server on Ubuntu
-            FileStream fstream = File.OpenRead("/home/alex/Desktop/secret_keys/password_bot.txt"); 
+            FileStream fstream = File.OpenRead("/root/secret_keys/password_bot.txt"); 
             //Test on Windows 
             //int lengthFstream = array.Length;
             //For server on Ubuntu
